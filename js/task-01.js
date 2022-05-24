f86314b9de6a77eabc1ecd7f91511a90f6f712e2
Vitalii-Seniura <senyura.w@gmail.com>
2022-05-24 23:28:31 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,5 +3,7 @@ const listEl = document.querySelectorAll('.item');
 console.log('Number of categories:', listEl.length);
 
 
-listEl.forEach(el => console.log('Category:', el.firstElementChild.textContent,
-'Elements:', el.lastElementChild.children.length));
+listEl.forEach(el => {
+    console.log('Category:', el.firstElementChild.textContent);
+    console.log('Elements:', el.lastElementChild.children.length);
+});
