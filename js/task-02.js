242bf8751bef98d5092c18fc0a180e5ea373cc28
Vitalii-Seniura <senyura.w@gmail.com>
2022-05-25 01:27:57 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector('ul');
+
+
+
+const elements = ingredients.map(el => {
+  const ingredient = document.createElement('li');
+  ingredient.textContent = el;
+  ingredient.classList.add('item');
+  
+  return ingredient;
+});
+
+
+
+ul.append(...elements);
+
