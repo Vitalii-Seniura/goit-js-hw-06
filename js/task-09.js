4a22b61e8c1dd8fd151b1006156434153b5fd972
Vitalii-Seniura <senyura.w@gmail.com>
2022-05-28 22:40:55 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const randomColor = document.querySelector(".change-color");
+const bodyColor = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+randomColor.addEventListener("click", bgcolor);
+
+
+function bgcolor() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+};
