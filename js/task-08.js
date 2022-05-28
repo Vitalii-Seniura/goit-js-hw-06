5759b2099c880e4f033a5fa0339d9f1af60c432a
Vitalii-Seniura <senyura.w@gmail.com>
2022-05-28 21:30:54 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert('All fields must be filled!!!');
+    } else {
+
+        console.log(`Login: ${email.value}, Password: ${password.value}`);
+        event.currentTarget.reset();
+    }
+}
